fe498ebebeabfce12505c8320f658874c998e4d2
Єлизавета Руда <lizarudayaa@gmail.com>
2021-04-27 03:49:06 +0300

    ModalWindow and design

---
+++ LizaModal/Login.js
@@ -0,0 +1,48 @@
+import Modal from "./Modal.js";
+
+export default function Login() {
+    class LoginModal extends Modal {
+        constructor({ id, classes }) {
+            super({ id, classes });
+        }
+        createFormElements() {
+            const login = document.createElement("input");
+            login.type = "text";
+            login.name = "login";
+            login.placeholder = "Login";
+            login.required = true;
+            login.classList = "modal_input"
+
+            const password = document.createElement("input");
+            password.type = "password";
+            password.name = "password";
+            password.placeholder = "Password";
+            password.required = true;
+            password.classList = "modal_input"
+
+            const submitBtn = document.createElement("button");
+            submitBtn.type = "submit";
+            submitBtn.textContent = "Login";
+            submitBtn.classList = "modal_login_button"
+
+            return [login, password, submitBtn];
+        }
+    }
+
+    const loginForm = new LoginModal({
+        id: "modalLogin",
+        classes: ["modal"],
+    });
+
+    const modal = document.getElementById("modal");
+    const loginBtn = document.getElementById("btn_log");
+
+
+
+    modal.append(loginForm.modal);
+    loginBtn.addEventListener("click", function (e) {
+        e.preventDefault();
+        loginForm.openModal();
+    });
+
+}
---
+++ LizaModal/Modal.js
@@ -0,0 +1,60 @@
+export default class Modal {
+    constructor({ id, classes }) {
+        this.id = id;
+        this.classes = classes;
+        this.modal = this.render()
+
+    }
+    render() {
+        const content = this.createForm(this.createFormElements());
+        const x = this.createElement({
+            elem: 'span',
+            content: 'x',
+            classes: ['close-modal']
+        });
+
+        x.addEventListener('click', () => this.closeModal());
+        const divModalContent = this.createElement({
+            elem: 'div',
+            classes: ['modal-content'],
+            content: [x, content]
+        });
+        const divModal = this.createElement({
+            elem: "div",
+            classes: this.classes,
+            content: [divModalContent],
+            id: this.id,
+        });
+        return divModal;
+    }
+    createElement({ elem, id, classes, content }) {
+        const element = document.createElement(elem);
+
+        if (id) {
+            element.id = id;
+        }
+        if (classes) {
+            element.classList.add(...classes);
+        }
+        if (content) {
+            element.append(...content)
+        }
+        return element
+    }
+    createForm(formElements = []) {
+        const form = document.createElement('form');
+        form.id = 'log-form';
+        form.action = "";
+        form.append(...formElements);
+        return form;
+    }
+
+    createFormElements() { }
+
+    openModal() {
+        this.modal.classList.add('active')
+    }
+    closeModal() {
+        this.modal.classList.remove('active')
+    }
+}
