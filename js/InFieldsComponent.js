36916a79253af042f1e38a525eaddac63527a175
УкрТехноСервис <uts.vlad@gmail.com>
2021-05-19 00:20:08 +0300

    Ну ОЧЕНЬ почти уже хороший код!

--- js/InFieldsComponent.js
+++ js/InFieldsComponent.js
@@ -1,48 +1,46 @@
 import changeDoctor from "./changeDoctor.js";
+
 export default class InFieldsComponent {
 
-    constructor(parent, {id, tag, containerClass, elementClass, elementType, elementName, labelText, placeholder, options} ) {
+    constructor(parent, {id, tag, ...args}) {
+        // id, tag, containerClass, elementClass, elementType, elementName, labelText, placeholder, options
         this.ES6classTitle = InFieldsComponent;
+        for (const [key, value] of Object.entries(args)) {
+            value && (this[key] = value)
+        }
+
         this.id = id;
         this.tag = tag;
-        this.containerClass = containerClass;
-        this.elementClass = elementClass;
-        elementType && (this.elementType = elementType);
-        elementName && (this.elementName = elementName);
-        placeholder && (this.placeholder = placeholder);
-        labelText   && (this.labelText     = labelText);
-        options     && (this.options = options);
-        // if (elementType) this.elementType = elementType;
-        // if (elementName) this.elementName = elementName;
-        // if (placeholder) this.placeholder = placeholder;
-        // if (labelText) this.labelText = labelText;
-        // if (options) this.options = options;
+
         this._DOMelements = {
             parent: parent,
             component: document.createElement('p'),
             selfEl: document.createElement(`${tag}`),
             labelEl: document.createElement("label"),
+        }
     }
-    }
+
     render() {
         const {parent, selfEl, labelEl, component} = this._DOMelements;
-        const { id, tag, containerClass, elementClass, labelText, options } = this;
+        const {tag, containerClass, elementClass, labelText, options} = this;
         if (tag === 'select') {
-            for (let i=0; i < options.length; i++) {
-                selfEl.add( new Option(options[i].text, options[i].value, options[i].defaultSelected, options[i].defaultSelected) );
+            for (let i = 0; i < options.length; i++) {
+                selfEl.add(new Option(options[i].text, options[i].value, options[i].defaultSelected, options[i].defaultSelected));
                 selfEl.size = options.length; // TODO  пока что раскрыли select полностью
-        }   }
+            }
+        }
 
         selfEl.className = elementClass;
-        if (this.elementName) selfEl.name = this.elementName;
-        if (this.elementType) selfEl.type = this.elementType;
-        if (this.placeholder) selfEl.placeholder = this.placeholder;
+        const {id, elementName, elementType, placeholder} = this;
+        if (elementName) selfEl.name = elementName;
+        if (elementType) selfEl.type = elementType;
+        if (placeholder) selfEl.placeholder = placeholder;
         selfEl.id = id;
-        if (this.id === "select-doctor"){
+        if (id === "select-doctor") {
             selfEl.addEventListener("click", (e) => {
-    //вызываем функцию вывода пустых формы для заполнения
+                //вызываем функцию вывода пустых формы для заполнения
                 changeDoctor(e, null, null); //если бы передали сюда второй и третий параметр-
-      // система перешла бы в режим редактирования карточки с номером в id
+                // система перешла бы в режим редактирования карточки с номером в id
             });
         }
 
@@ -54,7 +52,7 @@ export default class InFieldsComponent {
 
         labelEl.style.display = "block";
 
-        component.append(labelEl, selfEl );
+        component.append(labelEl, selfEl);
         parent.append(component);
     }
 }
--- js/WrapCardHTML.js
+++ js/WrapCardHTML.js
@@ -47,22 +47,17 @@ export default class WrapCardHTML {
         cardEl.append(...wantedElements);
 
         parent.append(cardEl);
-        // сюда перенести блок кнопок (см.выше), если хочу их видеть под карточкой.
         new CreateBtn(cardEl, showMoreCardBtnCfg).render();
         new CreateBtn(cardEl, editCardBtnCfg).render();
         new CreateBtn(cardEl, deleteCardBtnCfg).render();
 
-        // следующие 2стр кода: назначаем id всем только что созданным кнопкам:
         const showMoreBtnDOM = document.querySelectorAll(`div[data-name = "${id}"] button`);
-        //ниже сработает код с:  `${id}` );
         showMoreBtnDOM.forEach(btn=> btn.dataset.name = `${id}`);
-        //так же, как и сработал бы код с: id  =>  showMoreBtnDOM.forEach(btn=> btn.dataset.name = id);
     }
 
     /*** ФУНКЦИИ, назначаемые обработчикам КНОПОК каждой КАРТОЧКИ ***/
     static showMoreCardItems(e){
         const currentID = e.target.dataset.name; //получили id из сработавшей кнопки
-        // const currentCard = document.querySelector(`div[data-name = "${currentID}" ]`); //получили карточку с данным id
         const cardItems = document.querySelectorAll(`div[data-name = "${currentID}" ] .--hidden`); //получил все скрытые элементы внутри карточки с данным id
         cardItems.forEach(item=> item.classList.remove("--hidden"));
     };
@@ -70,7 +65,6 @@ export default class WrapCardHTML {
     static async deleteCard(e, id){//либо пришел e от кнопки "удалить карту" либо пришел id от процедуры изменения карточки fillFormFromCard
         let currentID;
         if (e) currentID = e.target.dataset.name; //получили id из сработавшей кнопки
-        // const currentCard = document.querySelector(`div[data-name = "${currentID}" ]`); //получили карточку с данным id
         else
             currentID = id;
         await Server.deleteCard(currentID, localStorage.getItem('token'));
@@ -81,7 +75,6 @@ export default class WrapCardHTML {
 
      static async editCard({target}){ //получили id карточки
         const currentID = target.dataset.name; //получили id из сработавшей кнопки
-        // const currentCard = document.querySelector(`div[data-name = "${currentID}" ]`); //получили карточку с данным id
         let card = await Server.getOneCard(currentID, localStorage.getItem('token')); //получили с сервера редактируемую карточку
         changeDoctor(target, currentID, card.doctor); //отослали редактируемуюу карточку на изменение в вызове формы
          window.scrollTo({
