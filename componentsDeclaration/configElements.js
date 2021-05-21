1573579560574c5a22681556677bc64094d0c935
Єлизавета Руда <lizarudayaa@gmail.com>
2021-05-21 09:11:24 +0300

    login btn

--- componentsDeclaration/configElements.js
+++ componentsDeclaration/configElements.js
@@ -1,6 +1,6 @@
 import VisitForm from "../js/visitForm.js";
 import WrapCardHTML from "../js/WrapCardHTML.js";
-import {showLoginBtn, hideSelectForm, deleteVisitForm} from "../js/createBtn.js"
+import { showLoginBtn, hideSelectForm, deleteVisitForm } from "../js/createBtn.js"
 
 /*** КНОПКИ КАРТОЧКИ***/
 
@@ -10,8 +10,8 @@ export const showMoreCardBtnCfg = {
     tag: "button",
     type: "button",  //TODO потом сменить на "submit"
     className: "card__show-more-btn btn btn-outline-success",
-    innerText:"Показать больше",
-    handler: (e)=>{
+    innerText: "Показать больше",
+    handler: (e) => {
         WrapCardHTML.showMoreCardItems(e);
     }
 };
@@ -22,8 +22,8 @@ export const editCardBtnCfg = {
     tag: "button",
     type: "button",  //TODO потом сменить на "submit"
     className: "card__edit-btn btn btn-outline-success",
-    innerText:"Редактировать карточку",
-    handler: (e)=>{
+    innerText: "Редактировать карточку",
+    handler: (e) => {
         WrapCardHTML.editCard(e);
     }
 };
@@ -34,8 +34,8 @@ export const deleteCardBtnCfg = {
     tag: "button",
     type: "button",
     className: "delete-btn btn btn-outline-success",
-    innerText:"Удалить карточку",
-    handler: (e)=>{
+    innerText: "Удалить карточку",
+    handler: (e) => {
         WrapCardHTML.deleteCard(e);//TODO  решить вопрос с непринятым промисом
     }
 };
@@ -51,7 +51,7 @@ export const createCardBtnCfg = {
     tag: "button",
     type: "button",
     className: "visit__create-btn  btn btn-outline-success return_btn",
-    innerText:"Создать карточку посетителя",
+    innerText: "Создать карточку посетителя",
     handler: VisitForm.formSubmitHandler
 };
 
@@ -62,8 +62,8 @@ export const resetBtnCfg = {
     tag: "button",
     type: "reset",
     className: "visit__reset-btn  btn  btn-outline-success return_btn",
-    innerText:"очистить форму",
-    handler: function formResetHandler(){}
+    innerText: "очистить форму",
+    handler: function formResetHandler() { }
 };
 
 
@@ -73,8 +73,8 @@ export const closeBtnCfg = {
     tag: "button",
     type: "button",
     className: "visit__close-btn btn-outline-success return_btn",
-    innerText:"Закрыть",
-    handler:  function cardCloseHandler(){
+    innerText: "Закрыть",
+    handler: function cardCloseHandler() {
         deleteVisitForm();
         hideSelectForm(); //выключили форму выбора врача
         showLoginBtn();
@@ -88,28 +88,28 @@ export const closeBtnCfg = {
 
 // конфигурируем select - Выбор специализации врача к посещению:  Терапевт, стоматолог, кардиолог
 export const doctorSelect = {
-        id: "select-doctor",
-        tag: "select",
-        containerClass: "visit__container",
-        elementClass: "visit__select",
-        elementName: "selectDoctor",
-        labelText:"Выберите Врача",
+    id: "select-doctor",
+    tag: "select",
+    containerClass: "visit__container",
+    elementClass: "visit__select",
+    elementName: "selectDoctor",
+    labelText: "Выберите Врача",
 
-        options: [
+    options: [
         {
-            id:0,
+            id: 0,
             value: "Кардиолог",
             text: "запись к Кардиологу",
             defaultSelected: false,
         },
         {
-            id:1,
+            id: 1,
             value: "Стоматолог",
             text: "запись к Стоматологу",
             defaultSelected: false,
         },
         {
-            id:2,
+            id: 2,
             value: "Терапевт",
             text: "запись к Терапевту",
             defaultSelected: false,
@@ -127,7 +127,7 @@ export const purpose = {
     elementName: "purpose",
     placeholder: "цель:",
     elementType: "text",
-    labelText:"Цель визита:",
+    labelText: "Цель визита:",
 };
 
 // краткое описание визита:
@@ -138,7 +138,7 @@ export const description = {
     elementClass: "visit__description",
     elementName: "description",
     placeholder: "--текст описания--",
-    labelText:"Опишите цель визита:",
+    labelText: "Опишите цель визита:",
 };
 
 // конфигурируем select -срочность визита:   обычная, приоритетная, неотложная
@@ -148,7 +148,7 @@ export const urgency = {
     containerClass: "visit__container",
     elementClass: "visit__urgency-select  al-c",
     elementName: "urgency",
-    labelText:"Задайте срочность посещения:",
+    labelText: "Задайте срочность посещения:",
 
     options: [
         {
@@ -180,7 +180,7 @@ export const lastName = {
     elementName: "lastName",
     placeholder: "фамилия",
     elementType: "text",
-    labelText:"введите Фамилию:",
+    labelText: "введите Фамилию:",
 };
 
 // конфигурируем Имя
@@ -192,7 +192,7 @@ export const mainName = {
     elementName: "mainName",
     placeholder: "имя",
     elementType: "text",
-    labelText:"введите Имя:",
+    labelText: "введите Имя:",
 };
 
 // конфигурируем Отчество
@@ -204,7 +204,7 @@ export const patrName = {
     elementName: "patrName",
     placeholder: "отчество",
     elementType: "text",
-    labelText:"введите Отчество:",
+    labelText: "введите Отчество:",
 };
 
 // конфигурация уникальных полей для заполнения к каждому из докторов
@@ -216,7 +216,7 @@ export const pressure = {
     elementName: "pressure",
     placeholder: "пример: 120х80",
     elementType: "text",
-    labelText:"Обычное давление посетителя:",
+    labelText: "Обычное давление посетителя:",
 };
 export const bodyWeightIndex = {
     id: "weight-index",
@@ -225,7 +225,7 @@ export const bodyWeightIndex = {
     elementClass: "visit__weight-index",
     elementName: "bodyWeightIndex",
     elementType: "number",
-    labelText:"Введите индекс массы тела:",
+    labelText: "Введите индекс массы тела:",
 };
 export const hadDeseases = {
     id: "visitor-deseases",
@@ -235,7 +235,7 @@ export const hadDeseases = {
     elementName: "hadDeseases",
     placeholder: "перенесённые болезни",
     elementType: "text",
-    labelText:"Перечислить перенесенные болезни:",
+    labelText: "Перечислить перенесенные болезни:",
 };
 export const age = {
     id: "visitor-age",
@@ -244,7 +244,7 @@ export const age = {
     elementClass: "visit__visitor-age",
     elementName: "age",
     elementType: "number",
-    labelText:"введите возраст посетителя:",
+    labelText: "введите возраст посетителя:",
 };
 
 // для Стоматолога:
@@ -256,31 +256,32 @@ export const lastVisitDate = {
     elementName: "lastVisitDate",
     placeholder: "формат: день.месяц.год",
     elementType: "date",
-    labelText:"введите дату последнего посещения:",
+    labelText: "введите дату последнего посещения:",
 };
 
 /*и теперь возьмём все константы выше и завернем их в один конфигурационный объект: */
-export const elemConfObj = {lastName: lastName, mainName: mainName, patrName: patrName, purpose: purpose, description: description, urgency: urgency,  pressure: pressure, bodyWeightIndex: bodyWeightIndex, hadDeseases: hadDeseases, age: age, lastVisitDate: lastVisitDate};
+export const elemConfObj = { lastName: lastName, mainName: mainName, patrName: patrName, purpose: purpose, description: description, urgency: urgency, pressure: pressure, bodyWeightIndex: bodyWeightIndex, hadDeseases: hadDeseases, age: age, lastVisitDate: lastVisitDate };
 
-export const login ={
+export const login = {
     type: 'text',
-    placeholder:'Email',
+    placeholder: 'Email',
     value: "uts_@ukr.net",
-    className:'modal_input',
+    className: 'modal_input',
     atr: 'text',
-    isRequierd : true
+    isRequierd: true
 }
 
 export const password = {
     type: 'password',
     placeholder: 'Password',
     value: "slyslysly",
-    className:'modal_input',
+    className: 'modal_input',
     atr: 'password',
-    isRequierd : true
+    isRequierd: true
 }
 export const btn = {
     type: 'submit',
-    className:'modal_login_button',
-    atr: 'submit'
+    className: 'modal_login_button',
+    atr: 'submit',
+    value: 'Login',
 }
